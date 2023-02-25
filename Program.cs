6640faef7bea79f954bafc8d5a0312efb830c140
seyed mohamad nouri sadeghi <125931486+smohamadns@users.noreply.github.com>
2023-02-25 20:49:52 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taklif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please insert the numbers :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("answer :"); Console.WriteLine(a + b);
+            Console.ReadKey();
+        }
+    }
+}
